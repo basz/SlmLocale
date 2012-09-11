bed19e43e565a124709ad7e022970ecac4ec3e83
Bas Kamer <bas@bushbaby.nl>
2012-09-12 00:43:05 +0200

    Implements UriPathStrategy plus tests

--- src/SlmLocale/Strategy/UriPathStrategy.php
+++ src/SlmLocale/Strategy/UriPathStrategy.php
@@ -43,14 +43,122 @@
 namespace SlmLocale\Strategy;
 
 use SlmLocale\LocaleEvent;
+use Zend\ServiceManager\ServiceManager;
+use Zend\ServiceManager\ServiceManagerAwareInterface;
 
-class UriPathStrategy extends AbstractStrategy
+class UriPathStrategy extends AbstractStrategy implements ServiceManagerAwareInterface
 {
+    const REDIRECT_STATUS_CODE = 302;
+
+    protected $redirectWhenFound = true;
+
+    protected $serviceManager;
+
+    public function setOptions(array $options = array())
+    {
+        if (array_key_exists('redirect_when_found', $options)) {
+            $this->redirectWhenFound = filter_var($options['redirect_when_found'], FILTER_VALIDATE_BOOLEAN);
+        }
+    }
+
+    /**
+     * Set service manager instance
+     *
+     * @param ServiceManager $locator
+     */
+    public function setServiceManager(ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+    }
+
     public function detect(LocaleEvent $event)
     {
+         $request = $event->getRequest();
+         if (!method_exists($request, 'getUri')) {
+            return;
+         }
+
+         $locale = $this->detectLocaleInRequest($event->getRequest());
+
+         if (!strlen($locale)) {
+             return;
+         }
+
+         if (!$event->hasSupported() || !in_array($locale, $event->getSupported())) {
+             return;
+         }
+
+         $router  = $this->serviceManager->get('router');
+         if (method_exists($router, 'getBaseUrl')) {
+             $baseurl = $router->getBaseUrl();
+             $router->setBaseUrl('/'. $locale);
+         }
+
+         return $locale;
     }
 
     public function found(LocaleEvent $event)
     {
+        $request = $event->getRequest();
+        if (!method_exists($request, 'getUri')) {
+           return;
+        }
+
+        $uri     = $event->getRequest()->getUri();
+        $locale  = $event->getLocale();
+
+        if (null === $locale) {
+           return;
+        }
+
+        $router  = $this->serviceManager->get('router');
+
+        if (method_exists($router, 'getBaseUrl')) {
+             $router->setBaseUrl('/'. $locale);
+        }
+
+         if ($locale == $this->detectLocaleInRequest($event->getRequest())) {
+             if (substr($uri->getPath(), -1) != '/') {
+                 $response = $event->getResponse();
+                 $location = $uri->toString() . '/';
+
+                $response->setStatusCode(self::REDIRECT_STATUS_CODE);
+                $response->getHeaders()->addHeaderLine('Location', $location);
+
+                $response->send();
+             }
+
+             return;
+         }
+
+         $uri->setPath('/' . $locale . $uri->getPath());
+
+        // if ($baseurl !== null) {
+             // $uri->setPath($uri->getPath() . ((substr($uri->getPath(), -1) == '/') ? '' : '/'));
+        // }
+
+        if (!$this->redirectWhenFound) {
+            return;
+        }
+
+        $location = $uri->toString();
+
+         $response = $event->getResponse();
+
+        $response->setStatusCode(self::REDIRECT_STATUS_CODE);
+        $response->getHeaders()->addHeaderLine('Location', $location);
+
+        $response->send();
+    }
+
+    protected function detectLocaleInRequest($request)
+    {
+         $uri    = $request->getUri();
+         $path   = $uri->getPath();
+         $parts  = explode("/", trim($path, '/'));
+         $locale = array_shift($parts);
+
+         return $locale;
+
     }
 }
---
+++ tests/SlmLocaleTest/Strategy/UriPathStrategyTest.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Copyright (c) 2012 Jurian Sluiman.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of the
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     SlmLocaleTest
+ * @author      Jurian Sluiman <jurian@juriansluiman.nl>
+ * @copyright   2012 Jurian Sluiman.
+ * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link        http://juriansluiman.nl
+ */
+namespace SlmLocaleTest\Locale;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use SlmLocale\LocaleEvent;
+use SlmLocale\Strategy\UriPathStrategy;
+use Zend\Console\Response as ConsoleResponse;
+use Zend\Console\Request as ConsoleRequest;
+use Zend\Http\PhpEnvironment\Response as HttpResponse;
+use Zend\Http\PhpEnvironment\Request as HttpRequest;
+use Zend\Mvc\Router\Http\TreeRouteStack as HttpRouter;
+use Zend\Mvc\Router\Console\SimpleRouteStack as ConsoleRouter;
+use Zend\ServiceManager\ServiceManager;
+
+class UriPathStrategyTest extends TestCase
+{
+    public function setup()
+    {
+        $this->strategy = new UriPathStrategy;
+        $this->strategy->setServiceManager($this->getServiceLocator());
+
+        $this->event = new LocaleEvent();
+        $this->event->setSupported(array('nl', 'de', 'en'));
+    }
+
+    /**
+     * Asserts that when the request instance does not has the getUri method detection is not possible as is the case for console requests
+     */
+    public function testDetect_ConsoleRequestReturnsNull()
+    {
+        $this->event->setRequest(new ConsoleRequest);
+        $this->event->setResponse(new ConsoleResponse);
+
+        $locale = $this->strategy->detect($this->event);
+
+        $this->assertNull($locale);
+    }
+
+    /**
+     * Asserts that no locale is detected with the default options
+     */
+    public function testDetect_NullByDefault()
+    {
+        $this->event->setRequest(new HttpRequest);
+        $this->event->setResponse(new HttpResponse);
+
+        $this->assertNull($this->strategy->detect($this->event));
+    }
+
+    /**
+     * Asserts that the first segment of a path is detected as locale with the default options
+     */
+    public function testDetect_FirstPathSegmentAsLocale()
+    {
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/en/deep/path/');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $this->assertEquals('en', $this->strategy->detect($this->event));
+    }
+    
+    public function testDetect_NullForUnsupported()
+    {
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/fr');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $this->assertNull($this->strategy->detect($this->event));
+    }
+
+    public function testDetect_SetsBaseUrlInRouter()
+    {
+        $serviceManager = $this->getServiceLocator();
+        $this->strategy->setServiceManager($serviceManager);
+
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/en');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->detect($this->event);
+
+        $this->assertEquals($serviceManager->get('router')->getBaseUrl(), '/en');
+    }
+
+    /**
+     * @runInSeparateProcess
+     * 'cause headers will be send (warning https://github.com/sebastianbergmann/phpunit/issues/254)
+     */
+    public function testFound_RedirectByDefault()
+    {
+        $this->event->setLocale('en');
+
+        $request = new HttpRequest;
+        $request->setUri('http://username:password@example.com:8080/some/deep/path/some.file?withsomeparam=true');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->found($this->event);
+
+        $this->assertEquals($this->event->getResponse()->getStatusCode(), 302);
+        $this->assertContains($this->event->getResponse()->getHeaders()->toString(),
+            "Location: http://username:password@example.com:8080/en/some/deep/path/some.file?withsomeparam=true\r\n");
+    }
+
+    /**
+     * @runInSeparateProcess
+     * 'cause headers will be send (warning https://github.com/sebastianbergmann/phpunit/issues/254)
+     */
+     public function testFound_ShouldRespectDisabledRedirectWhenFoundOption()
+     {
+        $this->strategy->setOptions(array('redirect_when_found' => false));
+        $this->strategy->setServiceManager($this->getServiceLocator());
+
+        $this->event->setLocale('en');
+        $this->event->setSupported(array('nl', 'de', 'en'));
+
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->found($this->event);
+
+        $this->assertNotEquals($this->event->getResponse()->getStatusCode(), 302);
+        $this->assertEquals($this->event->getResponse()->getHeaders()->toString(), "");
+    }
+
+    /**
+     * @runInSeparateProcess
+     * 'cause headers will be send (warning https://github.com/sebastianbergmann/phpunit/issues/254)
+     */
+    public function testFound_SetsBaseUrlInRouter()
+    {
+        $serviceManager = $this->getServiceLocator();
+        $this->strategy->setServiceManager($serviceManager);
+
+        $this->event->setLocale('en');
+        
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/en');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->found($this->event);
+
+        $this->assertEquals($serviceManager->get('router')->getBaseUrl(), '/en');
+    }
+
+    /**
+     * @runInSeparateProcess
+     * 'cause headers will be send (warning https://github.com/sebastianbergmann/phpunit/issues/254)
+     */
+    public function testFound_RedirectWhenAtLocaleUrlButMissingTrailingSlash()
+    {
+        $serviceManager = $this->getServiceLocator();
+        $this->strategy->setServiceManager($serviceManager);
+
+        $this->event->setLocale('en');
+        
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/en');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->found($this->event);
+
+        $this->assertEquals($this->event->getResponse()->getStatusCode(), 302);
+    }
+
+    /**
+     * @runInSeparateProcess
+     * 'cause headers will be send (warning https://github.com/sebastianbergmann/phpunit/issues/254)
+     */
+    public function testFound_DoesNotRedirectWhenAtLocaleUrl()
+    {
+        $serviceManager = $this->getServiceLocator();
+        $this->strategy->setServiceManager($serviceManager);
+
+        $this->event->setLocale('en');
+        
+        $request = new HttpRequest;
+        $request->setUri('http://example.com/en/');
+
+        $this->event->setRequest($request);
+        $this->event->setResponse(new HttpResponse);
+
+        $locale = $this->strategy->found($this->event);
+
+        $this->assertNotEquals($this->event->getResponse()->getStatusCode(), 302);
+    }
+
+    protected function getServiceLocator($withConsoleRouter=false)
+    {
+        $serviceLocator = new ServiceManager;
+        $serviceLocator->setService('router', $withConsoleRouter ? new ConsoleRouter : new HttpRouter);
+
+        return $serviceLocator;
+    }
+
+}
