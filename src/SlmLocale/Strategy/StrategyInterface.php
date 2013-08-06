a6e7267dbbf7f0125d00480a10e98dcaf8c4b4b4
Jurian Sluiman <jurian@soflomo.com>
2013-08-06 10:59:00 +0200

    Removal of package annotations

--- src/SlmLocale/Strategy/StrategyInterface.php
+++ src/SlmLocale/Strategy/StrategyInterface.php
@@ -32,8 +32,6 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * @package     SlmLocale
- * @subpackage  Strategy
  * @author      Jurian Sluiman <jurian@soflomo.com>
  * @copyright   2012-2013 Soflomo http://soflomo.com.
  * @license     http://www.opensource.org/licenses/bsd-license.php  BSD License
