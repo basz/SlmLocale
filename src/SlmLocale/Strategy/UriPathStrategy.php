5fb22891a8e5089477c1dba3e2f0a491410a0c8a
Jurian Sluiman <jurian@soflomo.com>
2013-08-15 10:57:52 +0200

    Cleanup uri path strategy to return response

--- src/SlmLocale/Strategy/UriPathStrategy.php
+++ src/SlmLocale/Strategy/UriPathStrategy.php
@@ -44,53 +44,87 @@ use SlmLocale\LocaleEvent;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
 use Zend\Stdlib\RequestInterface;
+use Zend\Mvc\Router\Http\TreeRouteStack;
 
 class UriPathStrategy extends AbstractStrategy implements ServiceLocatorAwareInterface
 {
     const REDIRECT_STATUS_CODE = 302;
 
-    protected $redirectWhenFound = true;
-
-    protected $serviceLocator;
+    protected $redirect_when_found = true;
+    protected $aliases;
+    protected $redirect_to_canonical;
+    protected $sl;
 
     public function setOptions(array $options = array())
     {
         if (array_key_exists('redirect_when_found', $options)) {
-            $this->redirectWhenFound = filter_var($options['redirect_when_found'], FILTER_VALIDATE_BOOLEAN);
+            $this->redirect_when_found = (bool) $options['redirect_when_found'];
+        }
+        if (array_key_exists('aliases', $options)) {
+            $this->aliases = (array) $options['aliases'];
+        }
+        if (array_key_exists('redirect_to_canonical', $options)) {
+            $this->redirect_to_canonical = (bool) $options['redirect_to_canonical'];
         }
     }
 
     /**
-     * Set service manager instance
-     *
-     * @param serviceLocator $locator
+     * {@inheritdoc}
      */
-    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    public function setServiceLocator(ServiceLocatorInterface $sl)
     {
-        $this->serviceLocator = $serviceLocator;
+        $this->sl = $sl;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getServiceLocator()
     {
-        return $this->serviceLocator;
+        return $this->sl;
+    }
+
+    protected function getRouter()
+    {
+        return $this->getServiceLocator()->getServiceLocator()->get('router');
+    }
+
+    protected function redirectWhenFound()
+    {
+        return $this->redirect_when_found;
+    }
+
+    protected function getAliases()
+    {
+        return $this->aliases;
+    }
+
+    protected function redirectToCanonical()
+    {
+        return $this->redirect_to_canonical;
     }
 
     public function detect(LocaleEvent $event)
     {
-        if (!$this->isHttpRequest($event->getRequest())) {
+        $request = $event->getRequest();
+        if (!$this->isHttpRequest($request)) {
             return;
         }
 
-        $router          = $this->getServiceLocator()->getServiceLocator()->get('router');
-        $existingBaseUrl = null;
-        if (method_exists($router, 'getBaseUrl')) {
-            $existingBaseUrl = $router->getBaseUrl();
+        $base   = null;
+        $router = $this->getRouter();
+        if ($router instanceof TreeRouteStack) {
+            $base = $router->getBaseUrl();
         }
 
-        $locale = $this->detectLocaleInRequest($event->getRequest(), $existingBaseUrl);
-
-        if (!strlen($locale)) {
+        $locale = $this->getFirstSegmentInPath($request, $base);
+        if (!$locale) {
             return;
+        }
+
+        $aliases = $this->getAliases();
+        if (null !== $aliases && array_key_exists($locale, $aliases)) {
+            $locale = $aliases[$locale];
         }
 
         if (!$event->hasSupported() || !in_array($locale, $event->getSupported())) {
@@ -102,7 +136,8 @@ class UriPathStrategy extends AbstractStrategy implements ServiceLocatorAwareInt
 
     public function found(LocaleEvent $event)
     {
-        if (!$this->isHttpRequest($event->getRequest())) {
+        $request = $event->getRequest();
+        if (!$this->isHttpRequest($request)) {
             return;
         }
 
@@ -111,51 +146,68 @@ class UriPathStrategy extends AbstractStrategy implements ServiceLocatorAwareInt
             return;
         }
 
-        $router          = $this->getServiceLocator()->getServiceLocator()->get('router');
-        $existingBaseUrl = null;
-        if (method_exists($router, 'getBaseUrl')) {
-            $existingBaseUrl = $router->getBaseUrl();
-            $router->setBaseUrl($existingBaseUrl . '/' . $locale);
-        }
-
-        $uri = $event->getRequest()->getUri();
-        if ($locale == $this->detectLocaleInRequest($event->getRequest(), $existingBaseUrl)) {
-            if ($router->getBaseUrl() == $uri->getPath()) {
-                $response = $event->getResponse();
-                $response->setStatusCode(self::REDIRECT_STATUS_CODE);
-                $response->getHeaders()->addHeaderLine('Location', $uri->toString() . '/');
-                $response->send();
+        if (!$this->redirectToCanonical() && null !== $this->getAliases()) {
+            $alias = $this->getAliasForLocale($locale);
+            if (null !== $alias) {
+                $locale = $alias;
             }
+        }
 
+        $base   = null;
+        $router = $this->getRouter();
+        if ($router instanceof TreeRouteStack) {
+            $base = $router->getBaseUrl();
+        }
+
+        $router->setBaseUrl($base . '/' . $locale);
+
+        $found = $this->getFirstSegmentInPath($request, $base);
+        if ($locale === $found) {
             return;
         }
 
-
-        $uri->setPath('/' . $locale . $uri->getPath());
-
-        if (!$this->redirectWhenFound) {
+        if (!$this->redirectWhenFound()) {
             return;
         }
+
+        $uri  = $request->getUri();
+        $path = $uri->getPath();
+
+        if (!$found || ($event->hasSupported() && !in_array($found, $event->getSupported()))) {
+            $path = '/' . $locale . $path;
+        } else {
+            $path = str_replace($found, $locale, $path);
+        }
+
+        $uri->setPath($path);
 
         $response = $event->getResponse();
         $response->setStatusCode(self::REDIRECT_STATUS_CODE);
         $response->getHeaders()->addHeaderLine('Location', $uri->toString());
-        $response->send();
+
+        return $response;
     }
 
-    protected function detectLocaleInRequest(RequestInterface $request, $baseurl = null)
+    protected function getFirstSegmentInPath(RequestInterface $request, $base = null)
     {
-        $uri    = $request->getUri();
-        $path   = $uri->getPath();
+        $path = $request->getUri()->getPath();
 
-        if ($baseurl) {
-            $path = substr($path, strlen($baseurl));
+        if ($base) {
+            $path = substr($path, strlen($base));
         }
 
-        $parts  = explode("/", trim($path, '/'));
+        $parts  = explode('/', trim($path, '/'));
         $locale = array_shift($parts);
 
         return $locale;
     }
 
+    protected function getAliasForLocale($locale)
+    {
+        foreach ($this->getAliases() as $alias => $item) {
+            if ($item === $locale) {
+                return $alias;
+            }
+        }
+    }
 }
