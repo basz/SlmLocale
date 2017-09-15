f67aabc585d12175582fadb515de0098f4fca09c
Vytautas Stankus <svycka@gmail.com>
2017-09-15 09:00:55 +0300

    Fixed cs issue and ServiceManager support

--- src/SlmLocale/Strategy/Factory/UriPathStrategyFactory.php
+++ src/SlmLocale/Strategy/Factory/UriPathStrategyFactory.php
@@ -65,8 +65,8 @@ class UriPathStrategyFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $strategy = new UriPathStrategy($container->getServiceLocator()->get('router'));
-        $strategy->setServiceLocator($container);
+        $strategy = new UriPathStrategy($container->get('router'));
+        $strategy->setServiceLocator($container->get(\SlmLocale\Strategy\StrategyPluginManager::class));
 
         return $strategy;
     }
@@ -80,6 +80,6 @@ class UriPathStrategyFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        return $this($serviceLocator, UriPathStrategy::class);
+        return $this($serviceLocator->getServiceLocator(), UriPathStrategy::class);
     }
 }
--- src/SlmLocale/Strategy/StrategyPluginManager.php
+++ src/SlmLocale/Strategy/StrategyPluginManager.php
@@ -40,9 +40,9 @@
 
 namespace SlmLocale\Strategy;
 
+use SlmLocale\Strategy\Factory\UriPathStrategyFactory;
 use Zend\ServiceManager\AbstractPluginManager;
 use Zend\ServiceManager\Factory\InvokableFactory;
-use SlmLocale\Strategy\Factory\UriPathStrategyFactory;
 
 class StrategyPluginManager extends AbstractPluginManager
 {
